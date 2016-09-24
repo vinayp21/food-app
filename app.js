201ecc2f7ad64368ed99a20610daca0016c60309
Vinay <vinprakash@deloitte.com>
2016-09-24 21:09:39 +0530

    updated mobile version

--- app.js
+++ app.js
@@ -21,11 +21,19 @@ var mymodal = angular.module('food', ['ngRoute']);
 		});
 	});
 
-	mymodal.controller('home', function($scope, $rootScope, sharedService){
+	mymodal.controller('home', function($scope, $rootScope, sharedService, orderService){
 		$scope.isSelected='home';
 		$rootScope.$on("setMenu", function(event, menu){
            $scope.selectedMenu(menu);
         });
+        $scope.$watch(function(){
+        	if(orderService.getOrders().length===0){
+			$scope.isCartEmpty=true;
+			}else{
+			$scope.isCartEmpty=false;
+			}
+        });
+		
 		$scope.selectedMenu= function(menuItem){
 			console.log(menuItem);
 			this.isSelected=menuItem;
